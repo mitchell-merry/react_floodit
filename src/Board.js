b919d996b107c45612701ee3bf2ad8a06c2e9d2b
diggitydingdong <mitchellinminecraft@gmail.com>
2021-06-06 02:36:40 +1000

    tweak to make the colour random - last commit for the night

--- src/Board.js
+++ src/Board.js
@@ -32,19 +32,20 @@ export default class Board extends Component {
         return row < 0 || col < 0 || row >= height || col >= width;
     }
 
-    floodFill_(grid, row, col, initialColour) {
-        grid[row][col].colour = 1; // TODO colour wheel
+    floodFill_(grid, row, col, initialColour, newColour) {
+        grid[row][col].colour = newColour; // TODO colour wheel
 
         for(let m = 1; m <= 7; m+=2) {
             let dr = Math.floor(m/3) - 1, dc = m%3 - 1;
-            if(!this.oob(row+dr, col+dc) && grid[row+dr][col+dc].colour === initialColour) this.floodFill_(grid, row+dr, col+dc, initialColour);
+            if(!this.oob(row+dr, col+dc) && grid[row+dr][col+dc].colour === initialColour) this.floodFill_(grid, row+dr, col+dc, initialColour, newColour);
         }
     }
 
     floodFill(row, col, initialColour) {
-        if(initialColour !== 1) {
+        let c = Math.floor(Math.random() * 6)+1;
+        if(initialColour !== c) {
             let {grid} = this.state;
-            this.floodFill_(grid, row, col, initialColour);
+            this.floodFill_(grid, row, col, initialColour, c);
 
             this.setState({grid});
         }
